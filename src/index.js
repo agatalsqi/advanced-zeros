293f5fce99dcd57efe79e73950dcb3325698e203
Agatalsqi <agatalsqi@gmail.com>
2018-02-27 01:38:32 +0300

    Solution 8-alpha

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  zeros = 0;
+  let i = 2;
+  while (i < base) {
+    if (base % i == 0) {
+      base /= i;
+    }
+    else {
+      i++;
+    }
+  }
+
+  while (number >= base) {
+    number = Math.floor(number / base);
+    zeros += number;
+  }
+  return zeros;
 }
