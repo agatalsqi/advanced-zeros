120780c60565b2f3b95f3ccbaa9498f3e825f737
Agatalsqi <agatalsqi@gmail.com>
2018-03-05 18:45:19 +0300

    Solution 8

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  zeros = 0;
-  let i = 2;
-  while (i < base) {
-    if (base % i == 0) {
-      base /= i;
-    }
+  var zeros = 0;
+  var dividers = primeFactorization(base);
+  base = dividers[0];
+  var power = dividers.length;
+  var i = 2;
+  
+  while (number >= base) {
+    number = Math.floor(number / base);
+    zeros += number;
+  }
+  return Math.floor(zeros / power);
+}
+
+function primeFactorization(number) {
+  var i = 2;
+  var maxDivider = 2;
+  var dividers = [];
+
+  while (i <= number) {
+    if (number % i == 0) {
+      if (i > 2) {
+        maxDivider = i;
+      }
+    dividers.push(i);
+    number /= i;
+  }
     else {
       i++;
     }
   }
 
-  while (number >= base) {
-    number = Math.floor(number / base);
-    zeros += number;
+  var maxDividers = dividers.filter(divider => divider == maxDivider);
+  var minDividers = dividers.filter(divider => divider == 2);
+
+  if (minDividers.length >= Math.pow(maxDivider, maxDividers.length)) {
+    return minDividers;
+  } else {
+    return maxDividers;
   }
-  return zeros;
 }
